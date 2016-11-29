38656e60be62d215354224efdb1edb4b81fb95d1
Warren <warren.henning@gmail.com>
2016-11-28 22:55:26 -0800

    New file for notes on stuff

--- code/learningc/learningc.c
+++ code/learningc/learningc.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <limits.h>
+#include <float.h>
 
 int main(void) {
     printf("int = %zd\n", sizeof(int));
@@ -7,5 +9,11 @@ int main(void) {
     printf("long long = %zd\n", sizeof(long long));
     printf("double = %zd\n", sizeof(double));
     printf("long double = %zd\n", sizeof(long double));
+    printf("FLT_MANT_DIG = %d\n", FLT_MANT_DIG);
+    printf("FLT_DIG = %d\n", FLT_DIG);
+    #pragma clang diagnostic push
+    #pragma clang diagnostic ignored "-Wdouble-promotion"
+    printf("FLT_EPSILON = %e\n", FLT_EPSILON);
+    #pragma clang diagnostic pop
     return 0;
 }
