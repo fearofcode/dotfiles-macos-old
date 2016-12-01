b71faa16595ddecb88ed2382d4641338da9fc662
Warren Henning <warren.henning@gmail.com>
2016-12-01 00:03:58 -0800

    Fun power series calculation

--- code/learningc/learningc.c
+++ code/learningc/learningc.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
+#include <float.h>
+#include <math.h>
+#include <stdlib.h>
 
-int main(void) {
-    char first[50], last[50];
-    scanf("%s %s", first, last);
-    printf("%s, %s\n", last, first);
+int main(int argc, char** argv) {
+    /* calculate 1 + 1/2 + 1/4 + 1/8 + ... */
+    double sum = 0.0;
+    unsigned long term_cnt = 0;
+    double denominator = 1.0;
+    double exponent;
+    
+    if (argc == 1) {
+	fprintf(stderr, "Supply an argument\n");
+	return 1;
+    }
+
+    exponent = strtod(argv[1], NULL);
+    printf("Using exponent %f\n", exponent);
+
+    while (fabs(1.0/denominator) > DBL_EPSILON) {
+	double term = 1.0 / denominator;
+	denominator *= exponent;
+	sum += term;
+	term_cnt++;
+	//printf("i = %lu: %f\n", term_cnt, sum);
+    }
+    printf("%f (%lu terms to converge)\n", sum, term_cnt);
     return 0;
 }
