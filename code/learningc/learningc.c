a3a70681c3033d1a286076887a91a5307305e5d9
Warren <warren.henning@gmail.com>
2016-12-04 20:09:50 -0800

    Oh look, I wrote some code

--- code/learningc/learningc.c
+++ code/learningc/learningc.c
@@ -1,29 +1,42 @@
 #include <stdio.h>
-#include <ctype.h>
 
-int main() {
-    unsigned int space_cnt = 0;
-    unsigned int newline_cnt = 0;
-    unsigned int other_ch_cnt = 0;
-    char ch;
+void swap(int* x, int* y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
 
-    while ((ch = getchar()) != '#') {
-	if (isspace(ch)) {
-	    space_cnt++;
-	}
-
-	if (ch == '\n') {
-	    newline_cnt++;
-	}
-
-	if (!isspace(ch) && ch != '\n') {
-	    other_ch_cnt++;
-	}
-
+void to_base_n(unsigned int k, unsigned int base) {
+    if (k >= base) {
+	to_base_n(k/base, base);
     }
 
-    printf("Spaces: %d\n", space_cnt);
-    printf("Newlines: %d\n", newline_cnt);
-    printf("Other characters: %d\n", other_ch_cnt);
+    printf("%d", k % base);
+}
+
+void iter_fib(unsigned int k) {
+    unsigned int a = 0;
+    unsigned int b = 1;
+    unsigned int c = a + b;
+    
+    for(int i = 1; i <= k; i++) {
+	printf("%d\n", c);
+	c = a + b;
+	a = b;
+	b = c;
+    }
+}
+
+int main() {
+    int x = 5, y = 10;
+    printf("before swap, x = %d, y = %d\n", x, y);
+    swap(&x, &y);
+    printf("after swap, x = %d, y = %d\n", x, y); 
+    to_base_n(5, 2);
+    printf("\n");
+    to_base_n(129, 8);
+    printf("\n");
+
+    iter_fib(20);
     return 0;
 }
