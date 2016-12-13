14cbf5f07fdb9dce1afe6ea0e521b32ab98c202c
Warren Henning <warren.henning@gmail.com>
2016-12-12 23:36:52 -0800

    Function pointer array example

--- code/learningc/learningc.c
+++ code/learningc/learningc.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
+#include <math.h>
 
-struct book {
-    const char * title;
-};
+void enumerate_values(double *arguments,
+		      size_t argument_len,
+		      size_t function_len,
+		      double (*functions[function_len])(double),
+		      const char   *function_names[function_len]) {
+    for(int arg_idx = 0; arg_idx < argument_len; arg_idx++) {
+	double arg = arguments[arg_idx];
 
-struct book make_a_book(const char *title) {
-    /* this uses designated initializers and compound literals, both C99 features */
-    return (struct book) {.title = title};
+	for(int fun_idx = 0; fun_idx < function_len; fun_idx++) {
+	    const char *function_name = function_names[fun_idx];
+	    double (*function)(double) = functions[fun_idx];
+	    printf("%s(%f) = %f\n", function_name, arg, function(arg));
+	}
+
+	printf("\n");
+    }
 }
 
 int main(int argc, char **argv) {
-    struct book b = make_a_book("TAOCP");
-    printf("%s\n", b.title);
+    double data[6] = {0.0, M_PI/4, 1.0, M_PI/2, M_PI, -1.0};
+    double (*functions[3])(double) = {sin, cos, tan};
+    const char *function_names[3] = {"sin", "cos", "tan"};
+
+    enumerate_values(data, 6, 3, functions, function_names); 
     return 0;
 }
