18cbd123190013a891fb15459bfa81a3e84de94f
Warren Henning <warren.henning@gmail.com>
2016-12-14 00:38:49 -0800

    Macro example

--- code/learningc/learningc.c
+++ code/learningc/learningc.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-    const int BYTE_MASK = 0xff;
-    const unsigned int color = 0x2a162f;
-    const unsigned int green = (color >> 8) & BYTE_MASK;
-    const unsigned int red = (color >> 16) & BYTE_MASK;
-    printf("green = %#x, red = %#x\n", green, red);
+#define SQR(X) ((X) * (X)) /* we are smart and efficient programmers! no function call! */
+    int k = 42;
+    #pragma clang diagnostic push
+    #pragma clang diagnostic ignored "-Wunsequenced"
+    printf("%d\n", SQR(++k));
+    #pragma clang diagnostic pop
     return 0;
 }
