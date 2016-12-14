de06c8566d1caab18ae2de83c17ef16faa1bfc87
Warren <warren.henning@gmail.com>
2016-12-13 21:21:45 -0800

    Remove stuff from Makefile I wasn't using, change code to use an array of structs containing a function pointer and a string

--- code/learningc/learningc.c
+++ code/learningc/learningc.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include <math.h>
 
+struct func {
+    double (*fp)(double);
+    const char * name;
+};
+
 void enumerate_values(double *arguments,
 		      size_t argument_len,
 		      size_t function_len,
-		      double (*functions[function_len])(double),
-		      const char   *function_names[function_len]) {
+		      struct func functions[function_len]) {
     for(int arg_idx = 0; arg_idx < argument_len; arg_idx++) {
 	double arg = arguments[arg_idx];
 
 	for(int fun_idx = 0; fun_idx < function_len; fun_idx++) {
-	    const char *function_name = function_names[fun_idx];
-	    double (*function)(double) = functions[fun_idx];
+	    const char *function_name = functions[fun_idx].name;
+	    double (*function)(double) = functions[fun_idx].fp;
 	    printf("%s(%f) = %f\n", function_name, arg, function(arg));
 	}
 
@@ -21,9 +25,13 @@ void enumerate_values(double *arguments,
 
 int main(int argc, char **argv) {
     double data[6] = {0.0, M_PI/4, 1.0, M_PI/2, M_PI, -1.0};
-    double (*functions[3])(double) = {sin, cos, tan};
-    const char *function_names[3] = {"sin", "cos", "tan"};
+    struct func functions[3] = {
+	{.fp = sin, .name = "sin"},
+	{.fp = cos, .name = "cos"},
+	{.fp = tan, .name = "tan"}
+    };
+    
+    enumerate_values(data, 6, 3, functions);
 
-    enumerate_values(data, 6, 3, functions, function_names); 
     return 0;
 }
