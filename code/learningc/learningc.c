a3480966687bba37dd8b3838f3cf7ec947abd2d2
Warren Henning <warren.henning@gmail.com>
2016-12-14 22:34:31 -0800

    babby's first variable-argument function

--- code/learningc/learningc.c
+++ code/learningc/learningc.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+double * new_arr(int n, ...) {
+    va_list ap;
+    va_start(ap, n);
+    
+    double *data = (double *) malloc(sizeof(double) * n);
+
+    for(int i = 0; i < n; i++) {
+	data[i] = va_arg(ap, double);
+    }
+
+    va_end(ap);
+    
+    return data;
+}
 
 int main(int argc, char **argv) {
-#define SQR(X) ((X) * (X)) /* we are smart and efficient programmers! no function call! */
-    int k = 42;
-    #pragma clang diagnostic push
-    #pragma clang diagnostic ignored "-Wunsequenced"
-    printf("%d\n", SQR(++k));
-    #pragma clang diagnostic pop
+    double * data = new_arr(5, 1.2, 3.4, 5.6, 6.8, -3.0);
+
+    for(int i = 0; i < 5; i++) {
+	printf("%d = %f\n", i, data[i]);
+    }
+    
     return 0;
 }
