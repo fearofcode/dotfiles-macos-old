bddbb19ad1a1b7d7c4713b7898dffefe88b179e6
Warren <warren.henning@gmail.com>
2016-12-24 17:23:14 -0800

    Why does Intermediate C Programming use pointer pointers for a list of structures? A regular old pointer that becomes an array seems to work

--- code/learningc/learningc.c
+++ code/learningc/learningc.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    int age;
+    char *name;
+} person;
+
+int compare_by_age(const void *p1, const void *p2) {
+    return ((const person *) p1)->age - ((const person *) p2)->age;
+}
 
 int main(void) {
-    const int rows = 8;
-    const int columns = 3;
+    person* people = malloc(sizeof(person)*2);
 
-    int **data = (int **) malloc(rows * sizeof(int *));
-    
-    for(int i = 0; i < rows; i++) {
-	data[i] = (int *) malloc(columns * sizeof(int));
-	
-	for(int j = 0; j < columns; j++) {
-	    data[i][j] = i + j;
-	    printf("data[%d][%d] = %d, &data[%d][%d] = %p\n", i, j, data[i][j], i, j, (void*)&data[i][j]);
-	}
+    people[0].age = 48;
+    people[0].name = strdup("test");
 
-	free(data[i]);
-    }
+    people[1].age = 42;
+    people[1].name = strdup("blah");
 
-    free(data);
+    printf("%d, %s\n", people[0].age, people[0].name);
+    printf("%d, %s\n", people[1].age, people[1].name);
+
+    qsort(people, 2, sizeof(person), compare_by_age);
+    printf("%d, %s\n", people[0].age, people[0].name);
+    printf("%d, %s\n", people[1].age, people[1].name);
+
+    free(people[1].name);
+    free(people[0].name);
+    free(people);
     return EXIT_SUCCESS;
 }
