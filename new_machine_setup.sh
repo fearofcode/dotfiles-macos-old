e1fb69b68569b2f1caae08f315898f7d28eac7a9
Warren Henning <warren.henning@gmail.com>
2019-06-02 16:46:33 -0700

    Add ripgrep, lsd, ctrlp, jellybeans

--- new_machine_setup.sh
+++ new_machine_setup.sh
@@ -3,7 +3,6 @@ set -euxo pipefail
 
 sudo apt install clang \
     curl \
-    dropbox \
     fonts-hack \
     fonts-inconsolata \
     fonts-ricty-diminished \
@@ -23,6 +22,26 @@ sudo apt install clang \
 curl https://sh.rustup.rs -sSf | sh
 source $HOME/.cargo/env
 
+cargo install ripgrep
+cargo install lsd
+
+# setup xclip to work like pbcopy/pbpaste for Mac
 echo "alias pbcopy='xclip -selection clipboard'" | tee -a ~/.bashrc
 echo "alias pbpaste='xclip -selection clipboard -o'" | tee -a ~/.bashrc
+echo "alias ls='lsd'" | tee -a ~/.bashrc
 
+# install jellybeans color theme
+mkdir -p ~/.vim/colors
+cd ~/.vim/colors
+curl -O https://raw.githubusercontent.com/nanotech/jellybeans.vim/master/colors/jellybeans.vim
+
+# install ctrlp
+cd ~/.vim
+git clone https://github.com/kien/ctrlp.vim.git bundle/ctrlp.vim
+
+cp .vimrc ~/.vimrc
+
+echo "Manual steps to perform:"
+echo " * Install Dropbox" # can technically be automated but whatever
+echo " * Get SSH keys from Dropbox"
+echo " * Install patched fonts for LSD at <https://github.com/ryanoasis/nerd-fonts/tree/master/patched-fonts>"
