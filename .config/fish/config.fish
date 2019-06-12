a57c54ee0615290224a80a5e4aedee458aa880f8
Warren Henning <warren.henning@gmail.com>
2019-06-11 17:30:44 -0700

    fix test condition and include venv auto activation logic

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,12 @@
 set PATH $HOME/.cargo/bin $HOME/.local/bin $PATH
 
 set is_linux (uname | grep Linux)
-if begin test -n $is_linux; and status --is-login; end
-    if test -z "$DISPLAY" -a $XDG_VTNR = 1
-        exec startx -- -keeptty
+
+if test -n "$is_linux"
+    if status --is-login
+        if test -z "$DISPLAY" -a $XDG_VTNR = 1
+            exec startx -- -keeptty
+        end
     end
 end
 
@@ -15,4 +18,21 @@ export LESS='--quit-if-one-screen --HILITE-UNREAD --tabs=4 --no-init --window=-4
 
 alias pbcopy='xclip -selection clipboard'
 alias pbpaste='xclip -selection clipboard -o'
+
+### BEGIN: AUTOVENV
+### Automatically activate a virtualenv stored in sub-directory called "venv"
+
+set git_root (git rev-parse --show-toplevel 2>&1)
+
+if test $status -eq 0
+    set activate_path "$git_root/venv/bin/activate.fish"
+
+    if test -e $activate_path
+        source $activate_path && export CD_VIRTUAL_ENV_ROOT=$git_root
+    end
+else if test -n "$CD_VIRTUAL_ENV_ROOT"
+    deactivate && unset CD_VIRTUAL_ENV_ROOT
+end
+
+### END: AUTOVENV
 
