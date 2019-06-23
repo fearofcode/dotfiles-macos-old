abd8e590f1af0b911a1758b7a416b1c1b25e7a8b
Warren Henning <warren.henning@gmail.com>
2019-06-23 01:20:49 -0700

    onedark colors for fzf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -24,6 +24,13 @@ alias ls='exa'
 # gives neat Ctrl-R history search widget, one of the few things I miss from bash
 fzf_key_bindings
 
+# you know I had to do it to 'em
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+--color=dark
+--color=fg:-1,bg:-1,hl:#c678dd,fg+:#ffffff,bg+:#4b5263,hl+:#d858fe
+--color=info:#98c379,prompt:#61afef,pointer:#be5046,marker:#e5c07b,spinner:#61afef,header:#61afef
+'
+
 ### BEGIN: AUTOVENV
 ### Automatically activate a virtualenv stored in sub-directory called "venv"
 
