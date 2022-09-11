d5bba70faf7c579511f80bb3bfa7029f3b6f49f8
Warren Henning <warren.henning@gmail.com>
2022-09-10 21:36:51 -0700

    simple approach to fixing login screen font size: make a custom lightdm greeter conf file

--- setup_high_dpi.sh
+++ setup_high_dpi.sh
@@ -2,3 +2,7 @@
 set -euxo pipefail
 echo "Xft.dpi: 192" | tee $HOME/.Xresources
 echo "xrdb -merge ~/.Xresources" | tee $HOME/.xinitrc
+
+# setup high dpi for login screen
+STARTING_DIR=$(pwd)
+sudo ln -sf $STARTING_DIR/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
