2ecfe65bbe4b259d8f7fa2b2e1ae70b302494195
Warren Henning <warren.henning@gmail.com>
2022-09-21 21:19:30 -0700

    fuck yeah nerd font icons

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -96,6 +96,8 @@ return packer.startup(function(use)
       vim.fn.sign_define('DapBreakpoint', {text='', texthl='', linehl='', numhl=''})
       vim.fn.sign_define('DapBreakpointCondition', {text='', texthl='', linehl='', numhl=''})
       vim.fn.sign_define('DapBreakpointRejected', {text='', texthl='', linehl='', numhl=''})
+      vim.fn.sign_define('DapStopped', {text='', texthl='', linehl='', numhl=''})
+      vim.fn.sign_define('DapLogPoint', {text='朗', texthl='', linehl='', numhl=''})
     end
   })
 
